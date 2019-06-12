e9f1e31f945dab322ffbaa4590561567c62815ed
ZhengYan49 <247728480@qq.com>
2019-06-12 14:20:03 +0800

    Update rule1.cpp

--- rule1.cpp
+++ rule1.cpp
@@ -1,7 +1,7 @@
-#ifndef _RULE1_H
-#define _RULE1_H
+
 int map[maxr][maxl], newmap[maxr][maxl];
 int m, n, general = 0;
+
 
 //初始化，生成随机数（无法避免随机数的浪费）
 void rule1()
@@ -11,5 +11,14 @@ void rule1()
 		for (int j = 0; j < n; j++)
 			map[i][j] = rand() % 2;        //假设约n/2
 }
-#endif
+int neighbor_num(int x, int y, int map[][maxl])
+{
+	int cnt = 0;
+	for (int i = 0; i < 8; i++)
+	{
+		int nx = x + dx[i], ny = y + dy[i];
+		if (nx >= 0 && nx < m && ny >= 0 && ny < n && map[nx][ny])  cnt++;
+	}
+	return cnt;
+}
 
